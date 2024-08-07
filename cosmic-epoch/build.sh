0212853abb596e1c110114a19ecf41b540f92cb6
Hadi Chokr <158838697+silverhadch@users.noreply.github.com>
2024-08-07 19:26:40 +0000

    Update build.sh
    Final update

--- cosmic-epoch/build.sh
+++ cosmic-epoch/build.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-#the claening tmp with sudo would bring temporary damage to system since cleaning without root will ignore important stuuf
-# the tmp ram usage is important for build time optimazaton
+#makepkg and other cannot be run as root
 if [ "$(id -u)" -eq 0 ]; then
     echo "Error: This script should not be run as root or with sudo."
     exit 1
@@ -8,7 +7,9 @@ fi
 # Will get the numbers of cpu cores for max power
 num_jobs=$(nproc)
 export MOLD_JOBS=1
-export CARGO_TARGET_DIR=/tmp/
+mkdir -p "$HOME/cargo-target"
+
+export CARGO_TARGET_DIR="$HOME/cargo-target"
 # List
 directories=(
   "cosmic-applets-git"
@@ -29,9 +30,6 @@ for dir in "${directories[@]}"; do
   if [ -d "$dir" ]; then
     cd "$dir"
     paru --mflags="-j${num_jobs}" -Ui
-    echo "Tempfiles are being deleted ignore the permission denied, these are important and will not be touched."
-    echo "The command will remove everything in tmp that is not protected, so dont worry"
-    rm -rf /tmp/*
     cd ..
   else
     echo "Directory $dir does not exist."
