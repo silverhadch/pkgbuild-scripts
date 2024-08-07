71a4c7b1b829fae9ffbc5e5544fd1aa13555037d
Hadi Chokr <158838697+silverhadch@users.noreply.github.com>
2024-08-07 19:01:12 +0000

    Update build.sh

--- cosmic-epoch/build.sh
+++ cosmic-epoch/build.sh
@@ -23,6 +23,9 @@ for dir in "${directories[@]}"; do
   if [ -d "$dir" ]; then
     cd "$dir"
     paru --mflags="-j${num_jobs}" -Ui
+    echo "Tempfiles are being deleted ignore the permission denied, these are important and will not be touched."
+    echo "The command will remove everything in tmp that is not protected, so dont worry"
+    rm -rf /tmp/*
     cd ..
   else
     echo "Directory $dir does not exist."
